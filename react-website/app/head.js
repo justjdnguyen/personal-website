88026cdb3aea664f7351d962ac6c5ea807e409b2
justjdnguyen <jimmynguyen2468@gmail.com>
2023-02-05 01:42:37 -0600

    The barebones of my personal website

---
+++ react-website/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Jimmy Nguyen</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Jimmy Nguyen Website" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
